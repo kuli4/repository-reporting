e450626076c01b01bf6f11451163018ebd656fb3
Sergey Kuleshov <zacerto92@mail.ru>
2020-12-26 18:43:31 +0300

    Fixed typo again

--- api-receiver/src/main/java/pro/kuli4/repository/apireceiver/ApiReceiverApplication.java
+++ api-receiver/src/main/java/pro/kuli4/repository/apireceiver/ApiReceiverApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRecieverApplication {
+public class ApiReceiverApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiRecieverApplication.class, args);
+        SpringApplication.run(ApiReceiverApplication.class, args);
     }
 }
